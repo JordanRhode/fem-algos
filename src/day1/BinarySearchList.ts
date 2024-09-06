6ad10da8dad1b71206b25d1181ac25ffe0c992e2
Jordan Rhode <2262659+JordanRhode@users.noreply.github.com>
2024-09-06 08:35:55 -0500

    Binary and Linear search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    return false;
+    let low = 0;
+    let high = haystack.length - 1;
+    while (true) {
+        if (low === high) return haystack[low] === needle;
+
+        // Find difference, divide by two, add low
+        const index = Math.round((high - low) / 2) + low;
+        const value = haystack[index];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = low === index ? low : index - 1;
+        } else if (value < needle) {
+            low = high === index ? high : index + 1;
+        }
+    }
 }
--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -2,5 +2,7 @@ export default function linear_search(
     haystack: number[],
     needle: number,
 ): boolean {
+    for (var i = 0; i < haystack.length; i++)
+        if (haystack[i] === needle) return true;
     return false;
 }
